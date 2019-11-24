7aeac33adc830f47c8215ee664a5a0af82786cf6
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-11-24 12:18:28 -0300

    Implementado Repositório Receita; Adicionado Provider Receita;

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:canhanho/repositories/receita.dart';
 import 'package:canhanho/repositories/usuario.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -6,7 +7,8 @@ import "routes.dart";
 void main() => runApp(
     MultiProvider(
         providers: [
-            ChangeNotifierProvider(builder: (context) => UsuarioModel())
+            ChangeNotifierProvider(builder: (context) => UsuarioModel()),
+            ChangeNotifierProvider(builder: (context) => ReceitaListModel()),
         ],
         child: MaterialApp(
             title: "Canhanho",
---
+++ lib/repositories/receita.dart
@@ -0,0 +1,107 @@
+import 'package:canhanho/repositories/base.dart';
+import 'package:canhanho/repositories/usuario.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+
+class Receita implements Base {
+	final String uid;
+	Timestamp data;
+	String descricao;
+	num valor;
+	final DocumentReference reference;
+
+	Receita({
+		this.data, this.descricao, this.valor,
+		this.uid = null, this.reference = null
+	});
+
+	Receita.fromMap(Map<String, dynamic> map, String id, {this.reference})
+		:	assert(map['data'] != null),
+			assert(map['descricao'] != null),
+			assert(map['valor'] != null),
+			uid			= id,
+			data		= map['data'],
+			descricao	= map['descricao'],
+			valor		= map['valor'];
+
+	Receita.fromSnapshot(DocumentSnapshot snapshot)
+		: this.fromMap(snapshot.data, snapshot.documentID, reference: snapshot.reference);
+
+	Map<String, dynamic> toMap() {
+		var dataMap = new Map<String, dynamic>();
+		dataMap['data'] = this.data;
+		dataMap['descricao'] = this.descricao;
+		dataMap['valor'] = this.valor;
+		return dataMap;
+	}
+
+	@override
+	String toString() => "Receita<$uid:$valor>";
+
+}
+
+class ReceitaListModel extends ChangeNotifier implements BaseListModel {
+
+	final Firestore _firestoreInstance = Firestore.instance;
+	final UsuarioModel _usuario = UsuarioModel();
+
+	final String _collection = "receitas";
+	static CollectionReference _reference;
+	static ObserverList<Receita> _list = null;
+
+	ReceitaListModel() {
+		if (_usuario.usuario != null) {
+			_reference = _firestoreInstance
+				.collection(
+				"${_collection}/${_usuario.usuario.uid}/${_collection}"
+			);
+		}
+	}
+
+	Future<ObserverList<Receita>> get() async {
+		if (_reference == null) {
+			_reference = _firestoreInstance
+				.collection(
+					"${_collection}/${_usuario.usuario.uid}/${_collection}"
+				);
+		}
+		_list = await _reference.getDocuments().then((query) {
+			return fromSnapshot(query.documents.toList());
+		});
+		notifyListeners();
+		return _list;
+	}
+
+	Future<ObserverList<Receita>> add(Base base) async {
+		Receita receita = base;
+		_reference.add(receita.toMap());
+		get();
+		return _list;
+	}
+
+	Future<ObserverList<Receita>> pop(Base base) async {
+		Receita receita = base;
+		_reference.document(receita.uid).delete();
+		get();
+		return _list;
+	}
+
+	Future<ObserverList<Receita>> update(Base base) async {
+		Receita receita = base;
+		var dataMap = new Map<String, dynamic>();
+		dataMap['valor'] = receita.valor;
+		_reference.document(receita.uid).setData(receita.toMap());
+		get();
+		return _list;
+	}
+
+	ObserverList<Receita> fromSnapshot(List<DocumentSnapshot> snapshots) {
+		var list = ObserverList<Receita>();
+		snapshots.forEach((snapshot) {
+			list.add(Receita.fromSnapshot(snapshot));
+		});
+		return list;
+	}
+
+}
