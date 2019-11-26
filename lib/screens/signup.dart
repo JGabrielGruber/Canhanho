00c992b8e61a010ae0fc1dc9f796fbdcdafeb091
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-11-26 16:54:58 -0300

    Implementada Tela de Cadastro de Usuário;

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,19 +1,23 @@
+import 'package:canhanho/repositories/usuario.dart';
+import 'package:canhanho/widgets/form_usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SignupScreen extends StatelessWidget {
+
 	@override
 	Widget build(BuildContext context) {
 		return Scaffold(
 			appBar: AppBar(
-				title: Text('Signup Screen'),
+				title: Text('Cadastrar conta'),
 			),
-			body: Center(
-				child: RaisedButton(
-					child: Text('Launch screen'),
-					onPressed: () {
-						// Navigate to the second screen when tapped.
-					},
-				),
+			body: Container(
+				padding: EdgeInsets.all(24.0),
+				child: FormUsuario(
+					isEdit: false,
+					userInfo: new User(),
+					updateInfo: Provider.of<UsuarioModel>(context).signUp,
+				)
 			),
 		);
 	}
