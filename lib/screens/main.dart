813b2903511a7aec014207afdfaec768548aa135
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-11-26 19:58:17 -0300

    Implementado base da Main Screen;

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,20 +1,63 @@
+import 'package:canhanho/screens/despesas.dart';
+import 'package:canhanho/screens/receitas.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: AppBar(
-				title: Text('Main Screen'),
-			),
-			body: Center(
-				child: RaisedButton(
-					child: Text('Launch screen'),
-					onPressed: () {
-						// Navigate to the second screen when tapped.
-					},
+		return DefaultTabController(
+			length: 3,
+			child: Scaffold(
+				appBar: AppBar(
+					title: GestureDetector(
+						onTap: () {
+							Navigator.of(context).pushNamed("/main/perfil");
+						},
+						child: Row(
+							crossAxisAlignment: CrossAxisAlignment.center,
+							children: <Widget>[
+								Icon(Icons.supervised_user_circle),
+								Text("Usuário")
+							],
+						),
+					),
+					bottom: TabBar(
+						tabs: [
+							Tab(
+								text: "Dashboard",
+								icon: Icon(Icons.dashboard),
+							),
+							Tab(
+								text: "Receitas",
+								icon: Icon(Icons.trending_up),
+							),
+							Tab(
+								text: "Despesas",
+								icon: Icon(Icons.trending_down),
+							)
+						]
+					),
 				),
-			),
+				body: TabBarView(
+					children: [
+						DashboardScreen(),
+						ReceitasScreen(),
+						DespesasScreen(),
+					]
+				),
+			)
 		);
 	}
+}
+
+class DashboardScreen extends StatefulWidget {
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Icon(Icons.dashboard);
+  }
 }
