93372da987310eedec75e611bcbd5aae39776073
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-11-28 00:57:34 -0300

    Implementada linha contendo totais;

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -5,6 +5,7 @@ import 'package:canhanho/repositories/usuario.dart';
 import 'package:canhanho/screens/despesas.dart';
 import 'package:canhanho/screens/receitas.dart';
 import 'package:charts_flutter/flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart' as prefix0;
@@ -78,8 +79,11 @@ class MainScreen extends StatelessWidget {
 class DashboardScreen extends StatelessWidget {
 	static List<Receita> _dataReceita = List<Receita>();
 	static List<Despesa> _dataDespesa = List<Despesa>();
+	static List<Base> _total = List<Base>();
 	static Series<Receita, DateTime> _receitas;
 	static Series<Despesa, DateTime> _despesas;
+	static Series<Base, DateTime> _totais;
+	static bool created = false;
 
 	DashboardScreen();
 
@@ -107,26 +111,30 @@ class DashboardScreen extends StatelessWidget {
 
 	/// Create one series with sample hard coded data.
 	static List<Series<Base, DateTime>> _createSeries(BuildContext context) {
+		if (!created) {
+			Provider.of<ReceitaListModel>(context).get().then((ObserverList<Receita> observerList) {
+				if (observerList.length != _dataReceita.length) {
+					observerList.forEach((Receita receita) {
+						if (_dataReceita.indexWhere((test) => test.uid == receita.uid) < 0) {
+							_dataReceita.add(receita);
+						}
+					});
+				}
+			});
 
-		Provider.of<ReceitaListModel>(context).get().then((ObserverList<Receita> observerList) {
-			if (observerList.length != _dataReceita.length) {
-				observerList.forEach((Receita receita) {
-					if (_dataReceita.indexWhere((test) => test.uid == receita.uid) < 0) {
-						_dataReceita.add(receita);
-					}
-				});
-			}
-		});
+			Provider.of<DespesaListModel>(context).get().then((ObserverList<Despesa> observerList) {
+				if (observerList.length != _dataDespesa.length) {
+					observerList.forEach((Despesa despesa) {
+						if (_dataDespesa.indexWhere((test) => test.uid == despesa.uid) < 0) {
+							_dataDespesa.add(despesa);
+						}
+					});
+				}
+			});
 
-		Provider.of<DespesaListModel>(context).get().then((ObserverList<Despesa> observerList) {
-			if (observerList.length != _dataDespesa.length) {
-				observerList.forEach((Despesa despesa) {
-					if (_dataDespesa.indexWhere((test) => test.uid == despesa.uid) < 0) {
-						_dataDespesa.add(despesa);
-					}
-				});
-			}
-		});
+			_total = _createTotais(_dataReceita, _dataDespesa);
+			created = true;
+		}
 
 		if (_receitas == null) {
 			_receitas = new Series<Receita, DateTime>(
@@ -148,10 +156,93 @@ class DashboardScreen extends StatelessWidget {
 			);
 		}
 
+		_totais = new Series<Base, DateTime>(
+			id: 'Totais',
+			colorFn: (_, __) => MaterialPalette.blue.shadeDefault,
+			domainFn: (item, _) {
+				var i;
+				if (item is Receita)
+					i = item as Receita;
+				else
+					i = item as Despesa;
+				return i.data.toDate();
+			},
+			measureFn: (item, _) {
+				var i;
+				if (item is Receita)
+					i = item as Receita;
+				else
+					i = item as Despesa;
+				return i.valor;
+			},
+			data: _total.length < 1 ? _createTotais(_dataReceita, _dataDespesa) : _total
+		);
+
 		return [
 			_receitas,
 			_despesas,
+			_totais
 		];
+	}
+
+	static List<Base> _createTotais(List<Receita> receita, List<Despesa> despesa) {
+		List<Base> total = List<Base>();
+		receita.forEach((item) {
+			total.add(new Receita(
+				uid: item.uid,
+				valor: item.valor,
+				data: item.data,
+				descricao: item.descricao
+			));
+		});
+		despesa.forEach((item) {
+			total.add(new Despesa(
+				uid: item.uid,
+				valor: item.valor,
+				data: item.data,
+				descricao: item.descricao
+			));
+		});
+		total.sort((a, b) {
+			var i, y;
+			if (a is Receita)
+				i = a as Receita;
+			else
+				i = a as Despesa;
+			if (b is Receita)
+				y = b as Receita;
+			else
+				y = b as Despesa;
+			return i.data.compareTo(y.data);
+		});
+		for(int index = 0; index < total.length; index++) {
+			var item = total.elementAt(index);
+			if (item is Receita) {
+				if (index != 0) {
+					var m = total.elementAt(index - 1), a;
+					if (m is Despesa)
+						a = m as Despesa;
+					else
+						a = m as Receita;
+					var r = item as Receita;
+					r.valor = a.valor + r.valor;
+				}
+			} else {
+				if (index != 0) {
+					var m = total.elementAt(index - 1), a;
+					if (m is Despesa)
+						a = m as Despesa;
+					else
+						a = m as Receita;
+					var d = item as Despesa;
+					d.valor = a.valor - d.valor;
+				} else {
+					var d = item as Despesa;
+					d.valor = 0 - d.valor;
+				}
+			}
+		}
+		return total;
 	}
 
 }
