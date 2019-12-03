988326a060a1330830294adee99ae31952a7b4b6
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-12-03 11:51:29 -0300

    Corrigido erro de sintaxe;

--- lib/screens/receitas.dart
+++ lib/screens/receitas.dart
@@ -1,4 +1,4 @@
-import 'package:canhanho/repositories/despesa.dart';
+import 'package:canhanho/repositories/receita.dart';
 import 'package:canhanho/widgets/base_controle.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -13,10 +13,10 @@ class _ReceitasScreenState extends State<ReceitasScreen> {
 	Widget build(BuildContext context) {
 
 		return ControleBase(
-			stream: Provider.of<DespesaListModel>(context).get().asStream(),
-			onAdd: Provider.of<DespesaListModel>(context).add,
-			onUpdate: Provider.of<DespesaListModel>(context).update,
-			type: () => Despesa(),
+			stream: Provider.of<ReceitaListModel>(context).get().asStream(),
+			onAdd: Provider.of<ReceitaListModel>(context).add,
+			onUpdate: Provider.of<ReceitaListModel>(context).update,
+			type: () => Receita(),
 		);
 	}
 }
