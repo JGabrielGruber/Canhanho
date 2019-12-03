2685aef48dafe9014bf84dbc2901975285dd0c6f
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-12-03 11:11:06 -0300

    Implementado Controle Base;

---
+++ lib/widgets/base_controle.dart
@@ -0,0 +1,76 @@
+import 'package:canhanho/repositories/base.dart';
+import 'package:canhanho/repositories/receita.dart';
+import 'package:canhanho/widgets/form_popup.dart';
+import 'package:canhanho/widgets/tabela.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class ControleBase extends StatefulWidget {
+	final Function onUpdate, onAdd;
+	final Stream<ObserverList<Base>> stream;
+	final type;
+
+
+	ControleBase({this.onUpdate, this.onAdd, this.stream, this.type});
+
+	@override
+	_ControleBaseState createState() => _ControleBaseState();
+}
+
+class _ControleBaseState extends State<ControleBase> {
+	@override
+	Widget build(BuildContext context) {
+
+		Future<void> _showForm({Base item = null}) async {
+			return showDialog<void>(
+				context: context,
+				barrierDismissible: true,
+				builder: (BuildContext context) {
+					if (item == null)
+						return FormBase(
+							updateInfo: widget.onAdd,
+							isEdit: false,
+							base: widget.type(),
+						);
+					return FormBase(
+						updateInfo: widget.onUpdate,
+						isEdit: true,
+						base: item,
+					);
+				},
+			);
+		}
+
+		return Scaffold(
+			resizeToAvoidBottomPadding: false,
+			body: SingleChildScrollView(
+				child: Column(
+					children: <Widget>[
+						StreamBuilder(
+							stream: widget.stream,
+							builder: (context, snapshot) {
+								if (snapshot.data != null)
+									return TableBase(
+										source: snapshot.data.toList(),
+										onSelected: (Base base) {
+											_showForm(item: base);
+										}
+									);
+								else
+									return Center(
+										child: CircularProgressIndicator(),
+									);
+							}
+						),
+						Padding(padding: EdgeInsets.all(30))
+					],
+				),
+			),
+			floatingActionButton: FloatingActionButton(
+				child: Icon(Icons.add),
+				onPressed: _showForm,
+				backgroundColor: widget.type is Receita ? Colors.green : Colors.red,
+			),
+		);
+	}
+}
