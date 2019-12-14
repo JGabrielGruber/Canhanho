d1c7b6bbca4adb1f44acd7b6fc06ade107198d66
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-12-13 21:35:01 -0300

    Implementado loading;

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,32 @@ import 'package:canhanho/repositories/despesa.dart';
 import 'package:canhanho/repositories/receita.dart';
 import 'package:canhanho/repositories/usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:load/load.dart';
 import 'package:provider/provider.dart';
 import "routes.dart";
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(
-    MultiProvider(
-        providers: [
-            ChangeNotifierProvider(builder: (context) => UsuarioModel()),
-            ChangeNotifierProvider(builder: (context) => ReceitaListModel()),
-            ChangeNotifierProvider(builder: (context) => DespesaListModel()),
-        ],
-        child: MaterialApp(
-            title: "Canhanho",
-            initialRoute: "/",
-            routes: routes,
-            localizationsDelegates: [
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
+    LoadingProvider(
+        child: MultiProvider(
+            providers: [
+                ChangeNotifierProvider(builder: (context) => UsuarioModel()),
+                ChangeNotifierProvider(builder: (context) => ReceitaListModel()),
+                ChangeNotifierProvider(builder: (context) => DespesaListModel()),
             ],
-            supportedLocales: [
-                const Locale('pt', 'BR'),
-            ],
-        ),
+            child: MaterialApp(
+                title: "Canhanho",
+                initialRoute: "/",
+                routes: routes,
+                localizationsDelegates: [
+                    GlobalMaterialLocalizations.delegate,
+                    GlobalWidgetsLocalizations.delegate,
+                    GlobalCupertinoLocalizations.delegate,
+                ],
+                supportedLocales: [
+                    const Locale('pt', 'BR'),
+                ],
+            ),
+        )
     )
 );
